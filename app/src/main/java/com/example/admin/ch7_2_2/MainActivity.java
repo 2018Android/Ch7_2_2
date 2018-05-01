f6b7ab65897141ecd11eab8f461eeb76ec89d2bd
chenyirou <annie851216@gmail.com>
2018-05-01 10:05:29 +0800

    將menu綁定至畫面中

--- app/src/main/java/com/example/admin/ch7_2_2/MainActivity.java
+++ app/src/main/java/com/example/admin/ch7_2_2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.admin.ch7_2_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
